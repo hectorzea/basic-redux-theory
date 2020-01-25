195d336a49088ba285208a15a4c47f33acc6923f
Hector <hzea@inclusionservices.com>
2020-01-24 21:32:52 -0300

    final

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {connect} from "react-redux";
+import Counter from "./Counter";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+let mapStateToProps = (state) => ({
+    countValue: state.count
+});
 
-export default App;
+// Action
+let increaseAction = {type: "increase"};
+let decreaseAction = {type: "decrease"};
+
+let mapDispatchToProps = (dispatch) => ({
+    increaseCount: () => {
+        return dispatch(increaseAction);
+    },
+    decreaseCount: () => {
+        return dispatch(decreaseAction)
+    }
+});
+
+// high order component <3
+let connectedComponent = connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(Counter);
+
+export default connectedComponent;
